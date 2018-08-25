f30644a17dbcfc55836d65f55db06aea92acde32
SimranAgrawal1 <42311837+SimranAgrawal1@users.noreply.github.com>
2018-08-25 15:39:36 +0530

    Pythagoran Triplet

--- Q9.java
+++ Q9.java
@@ -1,16 +1,14 @@
 public class PythagoranTriplet {
 
 	public static void main(String[] args) {
-		for(int a=1;a<1000;a++)
+		for(int c=1;a<1000;a++)
 		{
-			for(int b=a+1;b<1000;b++)
+			for(int b=1;b<1000;b++)
 			{
-				for(int c=b+1;c<1000;c++)
+				for(int a=1;c<1000;c++)
 				{
 						if((a*a+b*b==c*c) && (a+b+c==1000))
 						{
-		
-							
 								System.out.println("Pythagoran triplet is:"+a+" "+b+" "+c+" ");
 								System.out.println(a*b*c);
 								break;
